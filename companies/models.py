d46842c8b6eaa8c0fa7719d479e109ffc41d1f40
ghadiyaharsh <harshghadiya212@gmail.com>
2025-07-24 01:01:18 +0530

    admin and models

--- companies/models.py
+++ companies/models.py
@@ -5,6 +5,13 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    website = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
     # Add other company fields here...
 
     def __str__(self):
