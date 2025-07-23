baeb4b024fa6f7d9d4386aedec9ab694beddd445
Mithil Dabhi <mithildabhi898@gmail.com>
2025-07-23 18:38:55 +0530

    jobs and template

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'jobs'
+
 urlpatterns = [
     path('', views.main, name='main'),
+    path('jobs/', views.job_list, name='job_list'),
+    path('post-job/', views.post_job, name='post_job'),
+    path('applications/', views.view_applications, name='view_applications'),
     path('contact/', views.contact, name='contact'),
-    path('recruiter/dashboard/', views.recruiter_dashboard, name='recruiter_dashboard'),
-    path('recruiter/job_post/', views.post_job, name='post_job'),
-    path('recruiter/job/<int:job_id>/applicants/', views.view_applicants, name='view_applicants'),
-    path('recruiter/applicant/<int:application_id>/status/', views.update_application_status, name='update_application_status'),
 ]
