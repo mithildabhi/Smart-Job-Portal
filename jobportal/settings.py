e60c7368747fd64e36de4bf27a7642a39cf2a8f6
Mithil Dabhi <mithildabhi898@gmail.com>
2025-07-31 13:41:10 +0530

    Update settings for production readiness and add render configuration

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)k1s^to0emk)-d-zv652720%o13f&1-381cici#=n-nvneal8r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # or ['your-app-name.onrender.com']
 
 
 # Application definition
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',   
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jobportal.urls'
